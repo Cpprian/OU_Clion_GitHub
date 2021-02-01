b59af4c11399d266a14b89f0ad22810089d5317a
Cpprian <cyprian.szczepanski.css@gmail.com>
2021-02-01 20:24:59 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU_Clion_GitHub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_Clion_GitHub.dir/main.c.o"
+  "OU_Clion_GitHub"
+  "OU_Clion_GitHub.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OU_Clion_GitHub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, OU_Clion_GitHub!\n");
+    return 0;
+}
